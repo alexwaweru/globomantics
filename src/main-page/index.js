5a10acf2ab97acef1215e76aff7ecde3e517f75f
Alex Waweru <wawerua@kakbima.com>
2021-11-18 07:33:53 +0000

    react sample project

---
+++ src/main-page/index.js
@@ -0,0 +1,56 @@
+import { useEffect, useState, useMemo } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./main-page.css";
+import Header from "./header";
+import FeaturedHouse from "./featured-house";
+import SearchResults from "../search-results";
+import HouseFilter from "./house-filter";
+import HouseFromQuery from "../house/house-from-query";
+
+function App() {
+  const [allHouses, setAllHouses] = useState([]);
+
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
+
+  const featuredHouse = useMemo(() => {
+    if (allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+  }, [allHouses]);
+
+  return (
+    <Router>
+      <div className="container">
+        <Header />
+        <HouseFilter allHouses={allHouses} />
+        <Routes>
+          <Route
+            path="/searchresults/:country"
+            element={<SearchResults allHouses={allHouses}></SearchResults>}
+          ></Route>
+
+          <Route
+            path="/house/:id"
+            element={<HouseFromQuery allHouses={allHouses} />}
+          ></Route>
+
+          <Route
+            path="/"
+            element={<FeaturedHouse house={featuredHouse} />}
+          ></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
